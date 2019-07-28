bfc5c6ac25f71f5ba97dc991e7344b58a706401f
Max Heimes <max@pop-os.localdomain>
2019-07-28 17:43:50 +0100

    First use of pygame
    -removed unused imports
    -added game loop
    -set variables

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-import time
-import random
-print("Hello, World!")
+#Importing needed packages
+import pygame
+#Let user know the game is loading
+print("Initialising...")
+#Initialising the pygame library
+pygame.init()
+#Set Display resolution
+gameDisplay = pygame.display.set_mode((800,600))
+#set window name
+pygame.display.set_caption('Stick War')
+#Definitions
+clock = pygame.time.Clock()
+crashed = False
+#Game Loop
+while not crashed:
+    for event in pygame.event.get():
+        #If the program needs to close
+        if event.type == pygame.QUIT:
+            crashed = True
+        print(event)
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
+quit()
